5a3c351aa0895583248e2bfc84d2c70559ea9f5a
francisalexnwagbo <francisalexnwagbo@gmail.com>
2024-12-11 23:06:44 +0100

    Update to all files

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/francisalexnwagbo/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/francisalexnwagbo/flutter-keep" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.1.0+2" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 2 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/francisalexnwagbo/flutter"
+  "PROJECT_DIR=/home/francisalexnwagbo/flutter-keep"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/francisalexnwagbo/flutter-keep/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/francisalexnwagbo/flutter-keep/lib/main.dart"
+)
